463af6ba6fc82bf5b53d433175fe8307cb598b98
piippiip <71341479+piippiip@users.noreply.github.com>
2021-12-08 17:53:37 +0200

    Add files via upload

---
+++ Tokengeneration/generateAccessToken.js
@@ -0,0 +1,13 @@
+// $ npm i jsonwebtoken
+// ^ this should fetch the needed database to generate tokens using this code  + tokenAsignment
+
+const jwt = require("jsonwebtoken")
+
+function generateAccessToken (user) {
+    
+    return 
+    jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: "15m"})
+
+    }
+    
+    module.exports=generateAccessToken
